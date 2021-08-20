fa9f0ed43c342189b4f22072fec3dd4a8b7aab6f
Joshua Black <joshuablack.ecommerce@gmail.com>
2021-08-20 17:17:47 -0500

    DockerizedAPI

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,7 @@ namespace API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://localhost:5005");
+                    webBuilder.UseUrls("http://*:5005");
                     webBuilder.UseStartup<Startup>();
                 });
     }
